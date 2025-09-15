3431b16e8256952e55d7cf62a146493516dd39d7
Kaushik Gnanaskandan <kaushik.gnanas@gmail.com>
2025-09-15 16:00:00 -0700

    feat(core): finalize public API barrel exports

---
+++ packages/domscribe-core/src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * @domscribe/core - Core types, utilities, and constants for Domscribe
+ * @module @domscribe/core
+ */
+
+// Export all types
+export * from './lib/types/annotation.js';
+export * from './lib/types/manifest.js';
+export * from './lib/types/nullable.js';
+
+// Export utilities
+export {
+  generateEntryId,
+  generateAnnotationId,
+} from './lib/utils/id-generator.js';
+
+// Export errors
+export * from './lib/errors/index.js';
+
+// Export constants
+export * from './lib/constants/index.js';
+
+// Export migrations
+export * from './lib/migrations/annotation-migrations.js';
+
+// Export privacy patterns and redaction
+export * from './privacy/patterns.js';
+export { redactPII, redactSensitiveFields } from './privacy/redaction.js';
+export type { RedactionOptions } from './privacy/redaction.js';
+export * from './privacy/redaction-utils.js';
