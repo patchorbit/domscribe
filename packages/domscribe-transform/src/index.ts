980f8318fa4aa491057d5fea02dbd8e9461a3639
Kaushik Gnanaskandan <kaushik.gnanas@gmail.com>
2025-09-23 14:00:00 -0700

    feat(transform): finalize exports and subpath entries

---
+++ packages/domscribe-transform/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @domscribe/transform - AST injection of stable element IDs + bundler plugins
+ *
+ * Public API is exposed via subpath exports:
+ * - `@domscribe/transform/plugins/vite`
+ * - `@domscribe/transform/plugins/webpack`
+ * - `@domscribe/transform/webpack-loader` (string path for webpack config)
+ * - `@domscribe/transform/plugins/turbopack`
+ * - `@domscribe/transform/turbopack-loader` (string path for Next.js turbopack config)
+ *
+ * @module @domscribe/transform
+ */
